437cba04add244f5708c6906e6a451afec9bbc0e
Zack Balbin <zack@zackbalbin.com>
2022-08-10 13:49:20 -0700

    create basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { resolve_ergoname } from "ergonames";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [ergoName, setErgoName] = useState("~balb");
+  const [address, setAddress] = useState("");
+  const [resolved, setResolved] = useState(false);
+
+  async function handle_resolve() {
+    console.log(ergoName);
+    let resolved_address = await resolve_ergoname(ergoName);
+    console.log(resolved_address);
+    if (resolved_address == null) {
+      setResolved(false);
+      return;
+    } else {
+      setResolved(true);
+      setAddress(resolved_address);
+    }
+  };
+
+  function onTodoChange(value) {
+    setErgoName(value);
+    console.log(ergoName)
+  }
+
+  if (resolved) {
+    return (
+      <div className="bg-slate-800 text-white h-screen">
+        <h1 className="pt-8 text-2xl text-white text-center font-bold">ErgoNames Resolver</h1>
+        <div className="text-center">
+          <input onChange={e => onTodoChange(e.target.value)} className="mx-auto mt-4 w-5/6 p-2 border-2 border-white rounded-lg text-slate-800" type="text" placeholder="Enter an ErgoName" />
+          <button onClick={handle_resolve} className="mx-auto mt-4 w-5/6 p-2 border-2 border-white rounded-lg text-white hover:bg-slate-300 hover:text-slate-800 duration-300">Resolve</button>
+        </div>
+        <div className="text-center">
+          <button onClick={() => {navigator.clipboard.writeText(address)}} className="mx-auto h-24 mt-4 w-5/6 p-2 border-2 border-white rounded-lg text-white hover:bg-slate-300 hover:text-slate-800 duration-300 break-all">{address}</button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="bg-slate-800 text-white h-screen">
+        <h1 className="pt-8 text-2xl text-white text-center font-bold">ErgoNames Resolver</h1>
+        <div className="text-center">
+          <input onChange={e => onTodoChange(e.target.value)} className="mx-auto mt-4 w-5/6 p-2 border-2 border-white rounded-lg text-slate-800" type="text" placeholder="Enter an ErgoName" />
+          <button onClick={handle_resolve} className="mx-auto mt-4 w-5/6 p-2 border-2 border-white rounded-lg text-white hover:bg-slate-300 hover:text-slate-800 duration-300">Resolve</button>
+        </div>
+      </div>
+    );
+  }
+} 
 
 export default App;
